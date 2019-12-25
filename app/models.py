249d38f513c5c990db5c803d9a6bd3db53e25806
farisdidin <dinzen2304@gmail.com>
2019-12-25 16:47:15 +0700

    model and single repo

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db
 
 class Device(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    device_name = db.Column()
-    device_ip = db.Column()
-    device_repo_path = db.Column()
-    device_version = db.Column()
+    device_name = db.Column(db.String(100))
+    device_ip = db.Column(db.String(100))
+    device_repo_path = db.Column(db.String(100))
+    device_version = db.Column(db.String(100))
