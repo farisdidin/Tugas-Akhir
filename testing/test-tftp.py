871e827f30729fba793ce173df1e961a21f9e04f
farisdidin <dinzen2304@gmail.com>
2020-01-06 12:26:29 +0700

    Path config

--- testing/test-tftp.py
+++ testing/test-tftp.py
@@ -1,4 +1,4 @@
 import tftpy
 
 server = tftpy.TftpClient('10.199.16.68',69)
-server.upload('NX-ITSDC-A-20170807','NX-ITSDC-A-20170807')
+server.upload('NX-ITSDC-A-20170807','test_4/NX-ITSDC-A-20170807')
