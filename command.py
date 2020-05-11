55211fa82c3699ecb3f5fc0414954e178dc86f1c
farisdidin <dinzen2304@gmail.com>
2020-05-11 08:22:48 +0700

    remove gambar-git

--- command.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-command = ['git','--git-dir','./config/ftp/R1/.git', 'log']
-result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-result = result.stdout.decode('utf-8')
-print(result)
-print(type(result))
